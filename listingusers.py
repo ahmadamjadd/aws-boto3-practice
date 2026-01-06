cab1f90d4fa2ed1b60a5c57679512ab0fa379e6d
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-01-06 12:08:12 +0500

    experimenting with client

--- listingusers.py
+++ listingusers.py
@@ -2,6 +2,9 @@ import boto3
 
 aws_management_console = boto3.session.Session(profile_name = "default")
 resource = aws_management_console.resource("iam")
+client = aws_management_console.client("iam")
 
 for each_user in resource.users.all():
     print(each_user.name)
+
+print(client.list_users())
